a65a99375f6bbe765224951ce2cbbc8cf1b9ccf9
Carlos Vaz <carlosjvaz@gmail.com>
2016-10-31 07:37:34 -0200

    Android I - Backup da aula 3

--- android_I/projetos/Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ android_I/projetos/Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -18,6 +18,7 @@ public class FormularioActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(FormularioActivity.this, "Botão clicado!", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
     }
