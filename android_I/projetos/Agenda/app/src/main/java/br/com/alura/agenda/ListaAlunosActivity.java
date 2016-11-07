50b8cf8a87135de61f22ce2458b05f0a62f188d3
Carlos Vaz <carlosjvaz@gmail.com>
2016-11-07 07:31:32 -0200

    Android I - Finalizada a aula 4 Atualizando a lista na activity usando o onResume

--- android_I/projetos/Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ android_I/projetos/Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -20,13 +20,6 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        AlunoDAO dao = new AlunoDAO(this);
-        List<Aluno> alunos = dao.buscaAlunos(); dao.close();
-
-        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
-        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
-        listaAlunos.setAdapter(adapter);
-
         Button novoAluno = (Button) findViewById(R.id.novo_aluno);
         novoAluno.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +28,21 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 startActivity(intentVaiProFormuluraio);
             }
         });
+    }
+
+    private void carregaLista() {
+        AlunoDAO dao = new AlunoDAO(this);
+        List<Aluno> alunos = dao.buscaAlunos();
+        dao.close();
+
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregaLista();
     }
 }
