bcc465cf339b7c30a2784a445998844584f3f1a8
Carlos Vaz <carlosjvaz@gmail.com>
2016-11-16 06:15:57 -0200

    Android II - Iniciando aula 2 Salvando foto na pasta do aplicativo.

--- android_I/projetos/Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ android_I/projetos/Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -1,6 +1,8 @@
 package br.com.alura.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
 
 import br.com.alura.agenda.dao.AlunoDAO;
 import br.com.alura.agenda.modelo.Aluno;
@@ -28,6 +32,19 @@ public class FormularioActivity extends AppCompatActivity {
         if (aluno != null) {
             helper.preencheFormulario(aluno);
         }
+
+        Button botaoFoto = (Button) findViewById(R.id.formulario_botao_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                String caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivity(intentCamera);
+            }
+        });
+
     }
 
     @Override
