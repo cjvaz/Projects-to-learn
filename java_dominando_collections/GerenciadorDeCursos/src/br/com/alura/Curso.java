87deb964d16ce038d48d634aee14bdfed62cf6d1
Carlos Vaz <carlosjvaz@gmail.com>
2016-12-13 11:26:51 -0200

    correções

--- java_dominando_collections/GerenciadorDeCursos/src/br/com/alura/Curso.java
+++ java_dominando_collections/GerenciadorDeCursos/src/br/com/alura/Curso.java
@@ -23,6 +23,10 @@ public class Curso {
 		// retorna uma lista imutável ou read-only
 		// isso evita que seja adicionada um novo elemento na lista
 		// para fazer isso somente com o metodo adiciona!
+<<<<<<< 9b0bcd7e2183ac73fd506e684e1c6d0d088d55f5
+=======
+		// princípio da Programação defensiva.
+>>>>>>> Java: Dominando as Collections - 3. Relacionamentos com coleções
 		return Collections.unmodifiableList(aulas); 
 	}
 	
