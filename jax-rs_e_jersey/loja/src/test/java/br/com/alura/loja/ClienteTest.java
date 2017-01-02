2d9fc01887fa50673af911f38b8a6967f81a4b41
Carlos Vaz <carlosjvaz@gmail.com>
2017-01-01 23:05:56 -0200

    Jax-RS e Jersey: 05. Criando recursos e o POST

--- jax-rs_e_jersey/loja/src/test/java/br/com/alura/loja/ClienteTest.java
+++ jax-rs_e_jersey/loja/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -1,14 +1,13 @@
 package br.com.alura.loja;
 
-import java.net.URI;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -17,6 +16,7 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 public class ClienteTest {
 	
@@ -40,6 +40,22 @@ public class ClienteTest {
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 		
+	}
+	
+	@Test
+	public void testeCriarUmNovoCarrinhoComSucesso() {
+		Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        Carrinho carrinho = new Carrinho();
+        carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+        String xml = carrinho.toXML();
+        
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+        Response response = target.path("/carrinhos").request().post(entity);
+        Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));        
 	}
 
 }
