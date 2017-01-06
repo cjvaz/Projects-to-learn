4b1c0180ba6d07a945236c3d8ad9958fef23c294
Carlos Vaz <carlosjvaz@gmail.com>
2017-01-06 06:01:32 -0200

    Java EE: 01. Criando e Configurando o Projeto

---
+++ java_ee/casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -0,0 +1,36 @@
+package br.com.casadocodigo.loja.beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+import br.com.casadocodigo.loja.daos.LivroDao;
+import br.com.casadocodigo.loja.models.Livro;
+
+//CDI
+@Named
+@RequestScoped
+public class AdminLivrosBean {
+
+	private Livro livro = new Livro();
+	
+	// Context and Dependency Injection
+	@Inject
+	private LivroDao dao;
+
+	
+	@Transactional
+	public void salvar() {
+		dao.salvar(livro);
+		System.out.println("Livro cadastro: " + livro);
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+}
