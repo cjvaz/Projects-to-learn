dbd78069161a6644530c2c388333571c5791be72
Carlos Vaz <carlosjvaz@gmail.com>
2017-01-10 06:52:40 -0200

    Java EE

--- java_ee/casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ java_ee/casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,11 +1,15 @@
 package br.com.casadocodigo.loja.beans;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
 
 import br.com.casadocodigo.loja.daos.LivroDao;
+import br.com.casadocodigo.loja.models.Autor;
 import br.com.casadocodigo.loja.models.Livro;
 
 //CDI
@@ -24,6 +28,10 @@ public class AdminLivrosBean {
 	public void salvar() {
 		dao.salvar(livro);
 		System.out.println("Livro cadastro: " + livro);
+	}
+	
+	public List<Autor> getAutores() {
+		return Arrays.asList(new Autor(1, "Paulo Silveira"), new Autor(2, "Sergio Lopes"));
 	}
 
 	public Livro getLivro() {
