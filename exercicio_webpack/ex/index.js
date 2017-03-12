1149f4eb5fdff0c7794b3e8f7c44f51091213ac2
Carlos Vaz <carlosjvaz@gmail.com>
2017-03-12 18:35:50 -0300

    Seção 2, aula 10 - Usando EcmaScript 2015 sem Babel

--- exercicio_webpack/ex/index.js
+++ exercicio_webpack/ex/index.js
@@ -1,2 +1,12 @@
-const logger = require('./logger')
-logger.info('"Usando o padrão CommonJS!')
+class Pessoa {
+  constructor(nome) {
+    this.nome = nome;
+  }
+
+  toString() {
+    return `Pessoa: ${this.nome}`;
+  }
+}
+
+const pessoa = new Pessoa('Guilherme');
+console.log(pessoa.toString());
