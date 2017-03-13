8dc072c2467f5c25ecc5934f24874cf6d81a25a0
Carlos Vaz <carlosjvaz@gmail.com>
2017-03-13 18:42:13 -0300

    Seção 3, aula 24 - Componentes filhs

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SilvaFamily from './silvaFamily'
+import Family from './family'
+import Member from './member'
 
 ReactDOM.render(
-  <div>
-    <SilvaFamily />
-  </div>
+  <Family>
+    <Member name="Guilherme" lastName="Silva" />
+  </Family>
 , document.getElementById('app'))
