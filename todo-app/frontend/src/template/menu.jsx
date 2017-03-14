c6100d1096e81cc38d732670b20c7020fadeaa29
Carlos Vaz <carlosjvaz@gmail.com>
2017-03-14 17:42:50 -0300

    Seção 5, aula 45 - Configurando as rotas

--- todo-app/frontend/src/template/menu.jsx
+++ todo-app/frontend/src/template/menu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default props => (
   <nav className='navbar navbar-inverse bg-inverse'>
-    <div classnName='container'>
+    <div className='container'>
       <div className='navbar-header'>
         <a className='navbar-brand' href='#'>
           <i className='fa fa-calendar-check-o'></i>TodoApp
