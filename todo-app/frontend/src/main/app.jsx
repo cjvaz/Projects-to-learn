3840823717c6a05f30936634af2fe752411dcfee
Carlos Vaz <carlosjvaz@gmail.com>
2017-03-14 19:39:01 -0300

    Seção 5, aula 52 - Evento adicionar

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -4,14 +4,10 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import Menu from  '../template/menu'
 import Routes from './routes'
-import TodoForm from '../todo/todoForm'
-import TodoList from '../todo/todoList'
 
 export default props => (
   <div className='container'>
     <Menu />
     <Routes />
-    <TodoForm />
-    <TodoList />
   </div>
 )
