af9702c348be6e00cd6d39a2f9a23b487addf4eb
Carlos Vaz <carlosjvaz@gmail.com>
2017-03-16 03:41:35 -0300

    Seção 8, aula 90 - Criação da API REST

---
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,7 @@
+const BillingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({ new: true, runValidators: true })
+
+module.exports = BillingCycle
