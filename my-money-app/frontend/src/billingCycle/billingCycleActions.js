9f57a5a7db40b1fc1b1c1e97d4f6c05dcb2df4dd
Carlos Vaz <carlosjvaz@gmail.com>
2017-03-17 10:41:32 -0300

    Seção 9, aula 142 - Função botão cancelar

--- my-money-app/frontend/src/billingCycle/billingCycleActions.js
+++ my-money-app/frontend/src/billingCycle/billingCycleActions.js
@@ -4,6 +4,7 @@ import { reset as resetForm, initialize } from 'redux-form'
 import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
+const INITIAL_VALUES = {}
 
 export function getList() {
   const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -37,5 +38,14 @@ export function showUpdate(billingCycle) {
         showTabs('tabUpdate'),
         selectTab('tabUpdate'),
         initialize('billingCycleForm', billingCycle)
+    ]
+}
+
+export function init() {
+    return [
+        showTabs('tabList', 'tabCreate'),
+        selectTab('tabList'),
+        getList(),
+        initialize('billingCycleForm', INITIAL_VALUES)
     ]
 }
