39b952b652ab907e94f6741dd14394e0f5d266cb
Carlos Vaz <carlosjvaz@gmail.com>
2017-03-17 18:15:51 -0300

    Seção 9, aula 147 - Componente Input para CreditList

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 import { reduxForm, Field } from 'redux-form'
 
 import { init } from './billingCycleActions'
-import labelAndInput from '../common/form/labelAndInput'
+import LabelAndInput from '../common/form/labelAndInput'
 import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
@@ -14,16 +14,16 @@ class BillingCycleForm extends Component {
     return (
       <form role='form' onSubmit={handleSubmit}>
         <div className='box-body'>
-          <Field name='name' component={labelAndInput}
+          <Field name='name' component={LabelAndInput}
             label='Nome' cols='12 4' placeholder='Informe o nome'
             readOnly={readOnly} />
-          <Field name='month' component={labelAndInput} type='number'
+          <Field name='month' component={LabelAndInput} type='number'
             label='Mês' cols='12 4' placeholder='Informe o mês'
             readOnly={readOnly}  />
-          <Field name='year' component={labelAndInput} type='number'
+          <Field name='year' component={LabelAndInput} type='number'
             label='Ano' cols='12 4' placeholder='Informe o ano'
             readOnly={readOnly}  />
-          <CreditList cols='12 6' />
+          <CreditList cols='12 6' readOnly={readOnly} />
         </div>
         <div className='box-footer'>
           <button type='submit' className={`btn btn-${this.props.submitClass}`}>
