4fbed4daac3ac2563f12ffb6e7a93f8e3acb03a3
Carlos Vaz <carlosjvaz@gmail.com>
2017-03-17 04:20:46 -0300

    Seção 9, aula 134 - Exibindo os dados em BillingCycleList

--- my-money-app/frontend/src/billingCycle/billingCycleList.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleList.jsx
@@ -5,9 +5,20 @@ import { getList } from './billingCycleActions'
 
 class BillingCycleList extends Component {
 
-    componentWillMount() {
-      this.props.getList()
-    }
+  componentWillMount() {
+    this.props.getList()
+  }
+
+  renderRows() {
+    const list = this.props.list || []
+    return list.map(bc => (
+      <tr key={bc._id}>
+        <td>{bc.name}</td>
+        <td>{bc.month}</td>
+        <td>{bc.year}</td>
+      </tr>
+    ))
+  }
 
   render() {
     return (
@@ -21,7 +32,7 @@ class BillingCycleList extends Component {
             </tr>
           </thead>
           <tbody>
-
+            {this.renderRows()}
           </tbody>
         </table>
       </div>
