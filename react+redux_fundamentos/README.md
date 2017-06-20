4cb4ec5f2f766b4f772056b974b2b25635e5ee33
Carlos Vaz <carlosjvaz@gmail.com>
2017-06-19 23:32:50 -0300

    Restruturando e tornando o repositório para cursos da Udemy

---
+++ react+redux_fundamentos/README.md
@@ -0,0 +1,7 @@
+Curso: React + Redux: Fundamentos e Duas Apps do ABSOLUTO
+
+Instrutor: Leonardo Moura Leitao - Arquiteto de Software
+           https://twitter.com/leonardomleitao
+
+link: https://www.udemy.com/react-redux-pt/learn/v4/overview
+
---
+++ react+redux_fundamentos/my-money-app/frontend/src/main/routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
+
+import App from './app'
+import AuthOrApp from './authOrApp'
+import Dashboard2 from '../dashboard2/dashboard2'
+import BillingCycle from '../billingCycle/billingCycle'
+
+export default props => (
+  <Router history={hashHistory}>
+    <Route path='/' component={AuthOrApp}>
+      <IndexRoute component={Dashboard2} />
+      <Route path='/billingCycles' component={BillingCycle} />
+    </Route>
+    <Redirect from='*' to='/' />
+  </Router>
+)
