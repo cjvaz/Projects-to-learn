277ab808d9224dbb5b9dbc3645badd4e526bfade
Carlos Vaz <carlosjvaz@gmail.com>
2017-07-02 06:39:02 -0300

    Criando componente para exibir lista de contatos

--- angular2e4_typescript/01-cadastro-contatos/app/app.component.ts
+++ angular2e4_typescript/01-cadastro-contatos/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core'
 
 @Component({
-    selector: 'my-app',
-    template: `
+  selector: 'my-app',
+  template: `
       <h1>My first Angular 2</h1>
 
-      <button class="btn btn-success">Hello Bootstrap</button>
+      <contatos-lista></contatos-lista>
     `
 })
 export class AppComponent {
--- angular2e4_typescript/01-cadastro-contatos/app/app.module.ts
+++ angular2e4_typescript/01-cadastro-contatos/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContatosModule } from './contatos/contatos.module'
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [
-        AppComponent
-    ],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    ContatosModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
