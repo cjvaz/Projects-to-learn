d046df4d9ee3191a368ed7b913e389fab87dc583
Carlos Vaz <carlosjvaz@gmail.com>
2017-07-16 19:09:37 -0300

    App Frases do dia

--- dev+android+ios+react+redux/app2/index.android.js
+++ dev+android+ios+react+redux/app2/index.android.js
@@ -1,34 +1,51 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
 
 const estilos = {
-  estiloTexto: {
-    fontSize: 30,
-    backgroundColor: '#08509B',
-    height: 60,
-    width: 60
-  },
-  estiloTexto2: {
-    fontSize: 30,
-    backgroundColor: '#2A48FA',
-    height: 60,
-    width: 60
-  },
-  estiloView: {
-    backgroundColor: 'skyblue',
-    height: 300,
+  principal: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column'
+    alignItems: 'center'
+  },
+  botao: {
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 20
+  },
+  textoBotao: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
   }
 };
 
 export default class app2 extends Component {
+
+  gerarNovaFrase() {
+    const numeroAleatorio = Math.floor(Math.random() * 5);
+    const frases = [
+      '"O ponto de partida de qualquer conquista é o desejo" – Napoleon Hill, assessor político',
+      '"Se você está atravessando um inferno, continue atravessando" – Churchill',
+      '"Não tenha medo de desistir do bom para perseguir o ótimo" – John D. Rockefeller',
+      ' "Descobri que, quanto mais eu trabalho, mais sorte eu pareço ter" – Thomas Jefferson',
+      ' "Oportunidades não surgem. É você que as cria" – Chris Grosser, fotógrafo'
+    ];
+    Alert.alert(frases[numeroAleatorio]);
+  }
+
   render() {
+    const { principal, botao, textoBotao } = estilos;
+    const gerar = this.gerarNovaFrase;
     return (
-      <View style={estilos.estiloView}>
-        <Text style={estilos.estiloTexto}>A</Text>
-        <Text style={estilos.estiloTexto2}>B</Text>
+      <View style={principal}>
+        <Image source={ require('./imgs/logo.png') } />
+        <TouchableOpacity
+          onPress={gerar}
+          style={botao}
+        >
+          <Text style={textoBotao}>Nova Frase</Text>
+        </TouchableOpacity>
       </View>
     );
   }
