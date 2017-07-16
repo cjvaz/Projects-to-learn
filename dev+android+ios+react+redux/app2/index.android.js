0f65583c556f826f390fe4040c5f92a2f1e133f5
Carlos Vaz <carlosjvaz@gmail.com>
2017-07-16 14:33:23 -0300

    React Native - Adicionado o eslit para o vscode debugar o código

--- dev+android+ios+react+redux/app2/index.android.js
+++ dev+android+ios+react+redux/app2/index.android.js
@@ -1,53 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
+
+const estilos = {
+  estiloTexto: {
+    fontSize: 30,
+    backgroundColor: '#08509B',
+    height: 60,
+    width: 60
+  },
+  estiloView: {
+    backgroundColor: 'skyblue',
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  }
+};
 
 export default class app2 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={estilos.estiloView}>
+        <Text style={estilos.estiloTexto}></Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('app2', () => app2);
