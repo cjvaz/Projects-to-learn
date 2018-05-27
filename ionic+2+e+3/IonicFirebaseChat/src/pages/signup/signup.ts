d3c7882fec45a01b09aede1e0758ea84dbf098fa
Carlos Vaz <carlosjvaz@gmail.com>
2018-05-27 10:49:17 -0300

    ionic 2 e 3: Inserida a validação no signup

--- ionic+2+e+3/IonicFirebaseChat/src/pages/signup/signup.ts
+++ ionic+2+e+3/IonicFirebaseChat/src/pages/signup/signup.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -10,7 +10,19 @@ import { FormGroup } from '@angular/forms';
 export class SignupPage {
 
   signupForm: FormGroup;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public formBuilder: FormBuilder,
+    public navCtrl: NavController,
+    public navParams: NavParams) {
+
+      let emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+      this.signupForm = this.formBuilder.group({
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        username: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      })
   }
 
   onSubmit(): void {
