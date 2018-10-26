7294ce22da5d6358c5cc9e1b138f3cc1eb25e329
Carlos Vaz <carlos-vaz@procergs.rs.gov.br>
2018-10-26 17:20:25 -0300

    [react ninja] M2#A10 - map, quarto teste

--- react+ninja/m02/jest-in-pratice/src/map/map.js
+++ react+ninja/m02/jest-in-pratice/src/map/map.js
@@ -1,7 +1,11 @@
 "use strict";
 
-const map = arr => {
-  return arr;
+const map = (arr, func) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(func(arr[i]));
+  }
+  return newArr;
 };
 
 export default map;
