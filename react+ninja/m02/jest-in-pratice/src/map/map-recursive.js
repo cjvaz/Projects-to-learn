55aa4117d72b89a688f982add51ae71a2b362f55
Carlos Vaz <carlosjvaz@gmail.com>
2018-11-20 21:10:40 -0200

    [react ninja] M2#A13 - refactoring para ficar imutável

--- react+ninja/m02/jest-in-pratice/src/map/map-recursive.js
+++ react+ninja/m02/jest-in-pratice/src/map/map-recursive.js
@@ -10,17 +10,13 @@ const map = (arr = [], func = item => item) => {
     throw new TypeError("The second parameter must be a function");
   }
 
-  let counter = 0
-  const mapInternal = (arrayInternal) => {
-    if(arrayInternal.length === 0) {
-      return []
-    }
-
+  return (function mapInternal(arrayInternal, counter) {
     const [head, ...tail] = arrayInternal
-    return [func(head, counter++, arr)].concat(mapInternal(tail, func))
-  }
-
-  return mapInternal(arr)
+    return arrayInternal.length === 0 ? [] : [
+      func(head, counter, arr),
+      ...mapInternal(tail, counter + 1 )
+    ]
+  })(arr, 0)
 
 }
 
