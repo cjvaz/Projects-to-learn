343ed2d6bed09cdf4bde1d233a54b0eb4c69199c
Carlos Vaz <carlosjvaz@gmail.com>
2018-11-20 20:55:52 -0200

    [react ninja] M2#A13 - map recursivo parte 2

--- react+ninja/m02/jest-in-pratice/src/map/map-recursive.js
+++ react+ninja/m02/jest-in-pratice/src/map/map-recursive.js
@@ -1,11 +1,27 @@
 "use strict";
 
 const map = (arr = [], func = item => item) => {
-  if(arr.length === 0)
-    return []
 
-  const [head, ...tail] = arr
-  return [func(head)].concat(map(tail, func))
+  if (!Array.isArray(arr)) {
+    throw new TypeError("The first parameter must be an array");
+  }
+
+  if (typeof func !== "function") {
+    throw new TypeError("The second parameter must be a function");
+  }
+
+  let counter = 0
+  const mapInternal = (arrayInternal) => {
+    if(arrayInternal.length === 0) {
+      return []
+    }
+
+    const [head, ...tail] = arrayInternal
+    return [func(head, counter++, arr)].concat(mapInternal(tail, func))
+  }
+
+  return mapInternal(arr)
+
 }
 
 export default map;
