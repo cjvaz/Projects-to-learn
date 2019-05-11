222793794ee1230ccfe384d6795315728478abe7
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-11 19:22:13 -0300

    Angular I - A03P10: Ciclo de vida de um componente

--- cursos_javascript/angular_I/alurapic/src/app/app.component.ts
+++ cursos_javascript/angular_I/alurapic/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PhotoService } from './photos/photo/photo.service';
 
@@ -7,18 +7,19 @@ import { PhotoService } from './photos/photo/photo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   photos: Object[] = [];
 
   constructor(
-    photoService: PhotoService
-  ) {
-    photoService.listFromUser('flavio')
+    private photoService: PhotoService
+  ) {}
+
+  ngOnInit(): void {
+    this.photoService.listFromUser('flavio')
       .subscribe(
         photos => this.photos = photos,
         err => console.log(err.message)
       );
   }
-
 }
