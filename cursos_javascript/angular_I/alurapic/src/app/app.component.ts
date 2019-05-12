c0ff5fb22e86904beb419805b186194d0f271f64
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-12 18:40:59 -0300

    Angular I - A04P05: Organizando codigo e CommonModule

--- cursos_javascript/angular_I/alurapic/src/app/app.component.ts
+++ cursos_javascript/angular_I/alurapic/src/app/app.component.ts
@@ -1,25 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  photos: Object[] = [];
-
-  constructor(
-    private photoService: PhotoService
-  ) {}
-
-  ngOnInit(): void {
-    this.photoService.listFromUser('flavio')
-      .subscribe(
-        photos => this.photos = photos,
-        err => console.log(err.message)
-      );
-  }
-}
+export class AppComponent {}
