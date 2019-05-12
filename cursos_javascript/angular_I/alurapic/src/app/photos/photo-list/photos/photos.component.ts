fcf9d70cc7142dd86afcc64f65facc1970bc3c9b
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-12 20:22:46 -0300

    Angular I - A05P07: Change Detection

--- cursos_javascript/angular_I/alurapic/src/app/photos/photo-list/photos/photos.component.ts
+++ cursos_javascript/angular_I/alurapic/src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
@@ -6,16 +6,18 @@ import { Photo } from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = [];
   rows: any[] = [];
-  
+
   constructor() { }
 
-  ngOnInit() {
-    this.rows = this.groupColumns(this.photos);
-}
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
 
   groupColumns(photos: Photo[]) {
     const newRows = [];
