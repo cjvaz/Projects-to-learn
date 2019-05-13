8afd05409047af4a460cc5110ea1c10f74eb57df
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-13 09:20:53 -0300

    Angular I - A06P05: Resolvers

--- cursos_javascript/angular_I/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ cursos_javascript/angular_I/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
+
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -14,17 +14,11 @@ export class PhotoListComponent implements OnInit {
   filter = '';
 
   constructor(
-    private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService.listFromUser(userName)
-      .subscribe(
-        photos => this.photos = photos,
-        err => console.log(err.message)
-      );
+    this.photos = this.activatedRoute.snapshot.data.photos;
   }
 
 }
