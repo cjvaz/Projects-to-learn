095689a377b02b82be665c9eba3c9f6c5ce945b6
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-13 09:54:43 -0300

    Angular I - A06P07: RxJS e seu Subject

--- cursos_javascript/angular_I/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ cursos_javascript/angular_I/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(
     private activatedRoute: ActivatedRoute
@@ -19,6 +22,12 @@ export class PhotoListComponent implements OnInit {
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300)).subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
 }
