dc718adef860294cc7169802de96184c88dba5cb
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-15 09:52:14 -0300

    Angular II - A01P06: Componentizando mensagens de validação

--- cursos_javascript/angular_I/alurapic/src/app/home/home.module.ts
+++ cursos_javascript/angular_I/alurapic/src/app/home/home.module.ts
@@ -3,12 +3,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin/signin.component';
 import { CommonModule } from '@angular/common';
+import { VMessageComponent } from '../shared/components/vmessage/vmessage.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [ SigninComponent ],
     imports: [
       CommonModule,
-      ReactiveFormsModule
+      ReactiveFormsModule,
+      VMessageModule
     ]
 })
 export class HomeModule { }
