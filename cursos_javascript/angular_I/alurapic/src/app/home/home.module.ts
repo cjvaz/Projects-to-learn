edcdb5565dcb4f2398342fd2a45ff23632b6263e
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-15 09:40:01 -0300

    Angular II - A01P04: Validação de formulários

--- cursos_javascript/angular_I/alurapic/src/app/home/home.module.ts
+++ cursos_javascript/angular_I/alurapic/src/app/home/home.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { SigninComponent } from './signin/signin.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    declarations: [ SigninComponent ]
+    declarations: [ SigninComponent ],
+    imports: [
+      CommonModule,
+      ReactiveFormsModule
+    ]
 })
 export class HomeModule { }
