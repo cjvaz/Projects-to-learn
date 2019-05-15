79b834606321b7ce61c1d4c1f8fd38a21b089bf8
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-15 10:22:05 -0300

    Angular II - A01P08: Redirecionamento pós login

--- cursos_javascript/angular_I/alurapic/src/app/home/home.module.ts
+++ cursos_javascript/angular_I/alurapic/src/app/home/home.module.ts
@@ -5,13 +5,15 @@ import { SigninComponent } from './signin/signin.component';
 import { CommonModule } from '@angular/common';
 import { VMessageComponent } from '../shared/components/vmessage/vmessage.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [ SigninComponent ],
     imports: [
       CommonModule,
       ReactiveFormsModule,
-      VMessageModule
+      VMessageModule,
+      RouterModule
     ]
 })
 export class HomeModule { }
