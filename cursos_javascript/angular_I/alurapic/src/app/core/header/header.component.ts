81e2a4071840de7e1a3a190c8ec969ebf40cbe8b
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-18 15:04:38 -0300

    Angular II - A03P03:  O papel do BehaviorSubject

--- cursos_javascript/angular_I/alurapic/src/app/core/header/header.component.ts
+++ cursos_javascript/angular_I/alurapic/src/app/core/header/header.component.ts
@@ -1,8 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
+
+  user$: Observable<User>;
+  user: User;
+
+  constructor(
+    userService: UserService
+  ) {
+    this.user$ = userService.getUser();
+    this.user$.subscribe(user => this.user = user);
+  }
 }
