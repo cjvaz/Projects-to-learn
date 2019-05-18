1bd3d354db7dc73bf537f1d00a07cc7496ec9272
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-18 07:32:12 -0300

    Angular II - A02P02: Acesso do header de resposta

--- cursos_javascript/angular_I/alurapic/src/app/core/auth/auth.service.ts
+++ cursos_javascript/angular_I/alurapic/src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -15,6 +16,12 @@ export class AuthService {
   authenticate(userName: string, password: string) {
     return this.http.post(
       API_URL + '/user/login',
-      { userName, password });
+      { userName, password },
+      { observe: 'response'}
+      )
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        console.log(authToken);
+      }));
   }
 }
