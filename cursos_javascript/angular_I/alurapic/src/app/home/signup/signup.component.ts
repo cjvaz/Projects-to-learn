b3b1b6577c2d95b3d39b29928af70380ffcfffe6
Carlos Vaz <carlosjvaz@gmail.com>
2019-05-23 22:57:17 -0300

    Angular II - A04P08: Submissão dos dados

--- cursos_javascript/angular_I/alurapic/src/app/home/signup/signup.component.ts
+++ cursos_javascript/angular_I/alurapic/src/app/home/signup/signup.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { lowerCaseValidator } from '../../shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './signup.component.html'
@@ -12,7 +15,9 @@ export class SignUpComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private userNotTakenValidatorService: UserNotTakenValidatorService
+    private userNotTakenValidatorService: UserNotTakenValidatorService,
+    private signUpService: SignUpService,
+    private router: Router
     ) {}
 
   ngOnInit(): void {
@@ -40,5 +45,15 @@ export class SignUpComponent implements OnInit {
         [Validators.required, Validators.minLength(8), Validators.maxLength(14)]
       ]
     });
+  }
+
+  signup() {
+    const newUser = this.signupForm.getRawValue() as NewUser;
+    this.signUpService
+      .signup(newUser)
+      .subscribe(
+        () => this.router.navigate(['']),
+        err => console.log(err)
+      );
   }
 }
