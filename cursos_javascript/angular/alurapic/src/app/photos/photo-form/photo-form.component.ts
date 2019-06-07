5f0fe7bf6b4619c93f66488ceed8443c47478321
Carlos Vaz <carlosjvaz@gmail.com>
2019-06-07 05:50:09 -0300

    Angular III - A01P07: Upload da foto

--- cursos_javascript/angular/alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ cursos_javascript/angular/alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { PhotoService } from '../photo/photo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-photo-form',
@@ -13,7 +15,9 @@ export class PhotoFormComponent implements OnInit {
   file: File;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -28,9 +32,9 @@ export class PhotoFormComponent implements OnInit {
   upload() {
     const description = this.photoForm.get('description').value;
     const allowComments = this.photoForm.get('allowComments').value;
-    console.log(description);
-    console.log(allowComments);
-    console.log(this.file);
+    this.photoService
+      .upload(description, allowComments, this.file)
+      .subscribe(() => this.router.navigate(['']));
   }
 
 }
