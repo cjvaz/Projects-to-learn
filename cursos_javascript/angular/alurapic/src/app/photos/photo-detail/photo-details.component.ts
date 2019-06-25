1bf68451627c2d6bbd07b6b0d0c6dfda9c67ebfe
Carlos Vaz <carlosjvaz@gmail.com>
2019-06-24 21:15:14 -0300

    Angular III - A03P02: Rotas parametrizadas

---
+++ cursos_javascript/angular/alurapic/src/app/photos/photo-detail/photo-details.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: 'photo-details.component.html'
+})
+export class PhotoDetailsComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
