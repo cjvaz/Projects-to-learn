0c2052921860260dd4e1600cb2b84e6f74c46360
Carlos Vaz <carlos-vaz@procergs.rs.gov.br>
2020-02-07 13:46:55 -0300

    [tdd-java] - Adicionando o JUnit no projeto

--- tdd-java/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ tdd-java/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,6 +7,9 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class LocacaoService {
 	
@@ -28,7 +31,19 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void test() {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
 
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+
+		//verificacao
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }
